9d266c94daeb6d82e2fd90b7136aac72843a31bc
Dmitry <dmeetrygonchar@gmail.com>
2025-11-27 23:55:10 +0300

    feat(db): add db connection using prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { HealthCheckModule } from './health-check/health-check.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule, PrismaModule],
 })
 export class AppModule {}

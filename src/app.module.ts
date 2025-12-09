1f470f997d98167c228ee750e63d47852f82fa02
Dmitry <dmeetrygonchar@gmail.com>
2025-12-09 23:58:16 +0300

    feat: add CRUD for quiz, task entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { HealthCheckModule } from './health-check/health-check.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { QuizModule } from './quiz/quiz.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [HealthCheckModule, PrismaModule],
+  imports: [HealthCheckModule, PrismaModule, QuizModule, TaskModule],
 })
 export class AppModule {}
